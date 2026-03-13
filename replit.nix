72cc9062959ff6ecd4e9a9f5fd3a2bea7decc2f3
zachlextensoai <zacharie.laik.ext@lextenso.fr>
2026-03-13 16:47:04 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    (pkgs.tesseract.override { enableLanguages = [ "eng" "fra" ]; })
-    pkgs.poppler_utils
-    pkgs.ghostscript
-    pkgs.nodejs_20
-  ];
-}
